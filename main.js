0bbfd0874ea6a0956d2dc453b0e7fa453a2f60a2
Katy-fry <katyvfry@gmail.com>
2021-01-19 19:43:08 -0600

    array practice

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log("alice blue is named after one of the Roosevelt daughters");
+const students = ['Katy', 'Jackie', 'Sean', 'Nathan'];
+
+// console.log(students.length)
+
+// *****ACCESSING ITEMS IN ARRAY *******
+
+// console.log(students[students.length -1]); //Nathan
+// console.log(students[1]); //Jackie
+
+
+// students[0] = 'Matthew';
+// console.log(students); //reassigns katy to matthew
+
+
+// const valuePrinter = (array, index) => {
+//   return array[index];
+// }
+
+// console.log(valuePrinter(students, 2)); 
+
+
+// const inArray = (array, value) => {
+//   return array.includes(value)
+// }
+
+// console.log(inArray(students, "Katy"));
